e984255d140acc3aaf610a7f6f70663ded87b9e0
Administrator <2385342712@qq.com>
2020-01-05 18:16:34 +0800

    test3

--- demo-05-son/src/main/java/com/suo/App.java
+++ demo-05-son/src/main/java/com/suo/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "git test1" );
         System.out.println( "git test2" );
+        System.out.println( "git test3" );
     }
 }
