e2ded1f655ccb82e7b8b9fda502a48510992377b
Administrator <2385342712@qq.com>
2020-01-05 18:13:59 +0800

    test

---
+++ demo-05-son/src/main/java/com/suo/App.java
@@ -0,0 +1,14 @@
+package com.suo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "git test1" );
+    }
+}
---
+++ demo-05-son/src/test/java/com/suo/AppTest.java
@@ -0,0 +1,20 @@
+package com.suo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
